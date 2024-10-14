3c398296f5d34bf48b89f7698b0a9140b673f63a
keremuzer <keremuzer33@gmail.com>
2024-10-14 22:48:52 +0300

    add file I/O to SecretImage.cpp

--- SecretImage.cpp
+++ SecretImage.cpp
@@ -1,4 +1,8 @@
 #include "SecretImage.h"
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <sstream>
 
 // Constructor: split image into upper and lower triangular arrays
 SecretImage::SecretImage(const GrayscaleImage &image)
@@ -46,7 +50,8 @@ SecretImage::~SecretImage()
     // TODO: Your code goes here.
     // Simply free the dynamically allocated memory
     // for the upper and lower triangular matrices.
-    delete[] upper_triangular, lower_triangular;
+    delete[] upper_triangular;
+    delete[] lower_triangular;
 }
 
 // Reconstructs and returns the full image from upper and lower triangular matrices.
@@ -83,6 +88,24 @@ void SecretImage::save_back(const GrayscaleImage &image)
     // TODO: Your code goes here.
     // Update the lower and upper triangular matrices
     // based on the GrayscaleImage given as the parameter.
+    int upper_index = 0;
+    int lower_index = 0;
+    for (int i = 0; i < image.get_height(); i++)
+    {
+        for (int j = 0; j < image.get_width(); j++)
+        {
+            if (i >= j)
+            {
+                upper_triangular[upper_index] = image.get_pixel(i, j);
+                upper_index++;
+            }
+            else
+            {
+                lower_triangular[lower_index] = image.get_pixel(i, j);
+                lower_index++;
+            }
+        }
+    }
 }
 
 // Save the upper and lower triangular arrays to a file
@@ -95,6 +118,28 @@ void SecretImage::save_to_file(const std::string &filename)
     // If there are 15 elements, write them as: "element1 element2 ... element15"
     // 3. Write the lower_triangular array to the third line in a similar manner
     // as the second line.
+    std::ofstream output_file;
+    output_file.open(filename);
+
+    output_file << width + " " + height << std::endl;
+    for (int i = 0; i < width * (width + 1) / 2; i++)
+    {
+        output_file << upper_triangular[i];
+        if (i != (width * (width + 1) / 2) - 1)
+        {
+            output_file << " ";
+        }
+    }
+
+    for (int i = 0; i < width * (width - 1) / 2; i++)
+    {
+        output_file << lower_triangular[i];
+        if (i != (width * (width - 1) / 2) - 1)
+        {
+            output_file << " ";
+        }
+    }
+    output_file.close();
 }
 
 // Static function to load a SecretImage from a file
@@ -108,11 +153,30 @@ SecretImage SecretImage::load_from_file(const std::string &filename)
     // 5. Read the lower_triangular array from the third line, space-separated.
     // 6. Close the file and return a SecretImage object initialized with the
     //    width, height, and triangular arrays.
-    SecretImage secret_image(nullptr);
-    return secret_image;
+    std::ifstream input_file;
+    input_file.open(filename);
+    int width = 0, height = 0;
+    input_file >> width >> height;
+    int upper_size = width * (width + 1) / 2;
+    int lower_size = width * (width - 1) / 2;
+
+    int *upper_triangular = new int[upper_size];
+    int *lower_triangular = new int[lower_size];
+
+    for (int i = 0; i < upper_size; i++)
+    {
+        input_file >> upper_triangular[i];
+    }
+
+    for (int i = 0; i < lower_size; i++)
+    {
+        input_file >> lower_triangular[i];
+    }
+
+    input_file.close();
+    return SecretImage(width, height, upper_triangular, lower_triangular);
 }
 
-// Returns a pointer to the upper triangular part of the secret image.
 int *SecretImage::get_upper_triangular() const
 {
     return upper_triangular;
